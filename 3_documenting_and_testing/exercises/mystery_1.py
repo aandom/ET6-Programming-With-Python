cfd8b0b59a53fc6f1c2666399d5c9387c4fd1552
Alexander Andom <121858703+aandom@users.noreply.github.com>
2024-12-20 12:12:23 +0000

    documenting-and-testing-ex-mystery_1

--- 3_documenting_and_testing/exercises/mystery_1.py
+++ 3_documenting_and_testing/exercises/mystery_1.py
@@ -1,2 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on 20 12 24
+
+@author: Alexander Andom
+"""
+
 def mystery_1(a,b):
+    """Adds two numbers and return the result 
+
+    Parameters:
+        a: int, any integer
+        b: int, any integer
+    
+    Returns -> int the result of sum of the numbers a and b
+    >>> mystery_1(1, 0)
+    1
+
+    >>> mystery_1(-1, 1)
+    0
+
+    >>> mystery_1(8, 19)
+    27
+    """
+    assert isinstance(a, int), "first argument should be an integer"
+    assert isinstance(b, int), "second argument should be an integer"
     return a + b
