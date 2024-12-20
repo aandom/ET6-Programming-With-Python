92797f11e2bb42a9329978f3641237f8e7c14d4e
Alexander Andom <121858703+aandom@users.noreply.github.com>
2024-12-20 12:13:00 +0000

    documenting-and-testing-ex-mystery_2

--- 3_documenting_and_testing/exercises/mystery_2.py
+++ 3_documenting_and_testing/exercises/mystery_2.py
@@ -1,4 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on 20 12 2024
+
+@author: Alexander Andom
+"""
+
+
+
 def mystery_2(a):
+    """Returns the length of the input if it is non-empty.
+
+    Parameters:
+        a: Any iterable (e.g., list, string, tuple).
+
+    Returns:
+        int: The length of the iterable `a`, or None if `a` is empty.
+
+    Examples:
+        >>> mystery_2("hello")
+        5
+
+        >>> print(mystery_2([]))
+        None
+
+        >>> mystery_2([1, 2, 3])
+        3
+    """
     if len(a) == 0:
         return None
 
